b1901423fa73a8783eb2752582f19d8095bbb656
Rafiul Ferdous <rafiulferdous434@gmail.com>
2026-03-13 21:20:03 +0600

    sanctum auth implimented

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,15 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/products/dashboard', [ProductController::class, 'dashboard']);
+    Route::get('/products/sales-report', [ProductController::class, 'salesReport']);
+});
+
 Route::get('/products', [ProductController::class, 'index']);
 Route::post('/products', [ProductController::class, 'store']);
 Route::get('/products/search', [ProductController::class, 'search']);
-Route::get('/products/dashboard', [ProductController::class, 'dashboard']);
-Route::get('/products/sales-report', [ProductController::class, 'salesReport']);
+
 
 Route::get('/orders', [OrderController::class, 'index']);
 Route::post('/orders', [OrderController::class, 'store']);
